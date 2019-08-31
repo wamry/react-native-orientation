ec38ec5d9d39ddbf17850676638c43b56cf16067
Omar Wamry <andromedaxero@gmail.com>
2019-08-31 22:16:41 +0200

    update react-native-config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    project: {
-        ios: {
-            project: 'iOS/RCTOrientation.xcodeproj',
-        },
-        android: {
-            "packageInstance": "new OrientationPackage()"
+    dependency: {
+        platforms: {
+            ios: {
+	        project: 'iOS/RCTOrientation.xcodeproj',
+	    },
+	    android: {
+	        "packageInstance": "new OrientationPackage()"
+	    }
         }
     }
 };
